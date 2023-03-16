a5a5feb0dc8f362b8138fd8b10a1e1b45cbe5452
Adam La Rosa <adam.larosa@flatironschool.com>
2023-03-16 09:20:50 -0700

    013023 solution

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,48 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 class CashRegister:
-  pass
+
+  def __init__( self, discount_value = 0 ):
+    self.discount = discount_value
+    self.total = 0
+    self.items = []
+    
+    
+  def add_item( self, item_string, price_float, quantity = 1 ):
+    
+    new_total = price_float * quantity
+    self.last_transaction = new_total
+    
+    for i in range( quantity ):
+      self.items.append( item_string )
+    self.total += new_total
+    
+  def apply_discount( self ):
+    if self.discount > 0:
+      percent = self.discount / 100
+      money_off = self.total * percent
+      self.total = int( self.total - money_off )
+      print( f"After the discount, the total comes to ${self.total}." )
+    else:
+      print( "There is no discount to apply." )
+   
+
+  def void_last_transaction( self ):
+    self.total = self.total - self.last_transaction
+  
+
+
+
+
+
+
+c1 = CashRegister()
+
+c1.add_item( "macbook air", 1000 )
+
+c1.apply_discount()
+
+
+
